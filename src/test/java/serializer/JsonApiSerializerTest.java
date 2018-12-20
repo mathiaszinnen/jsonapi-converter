ad7454f27b3038b34974ea24a43be57077f44a25
Mathias Zinnen <mathias.zinnen@gmail.com>
2018-12-20 22:51:09 +0100

    implement collection serialization

--- src/test/java/serializer/JsonApiSerializerTest.java
+++ src/test/java/serializer/JsonApiSerializerTest.java
@@ -10,6 +10,9 @@ import document.JsonApiDocument;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonApiSerializerTest {
@@ -27,11 +30,15 @@ public class JsonApiSerializerTest {
     private static class SimplePojo {
 
         @JsonApiId
-        public final String id = "id";
+        public final String id;
         public String anotherAttribute = "something";
         public int yetAnother = 42;
+
+        private SimplePojo(String id) {
+            this.id = id;
+        }
     }
-    private final SimplePojo simple = new SimplePojo();
+    private final SimplePojo simple = new SimplePojo("id");
 
     @Test
     void testSimplePojoSerialization() {
@@ -44,6 +51,24 @@ public class JsonApiSerializerTest {
         assertEquals(2, result.get("data").get("attributes").size());
         assertEquals("something", result.get("data").get("attributes").get("anotherAttribute").textValue());
         assertEquals(42, result.get("data").get("attributes").get("yetAnother").asInt());
+    }
+
+    @Test
+    void testCollectionSerialization() {
+        List<SimplePojo> pojoList = Arrays.asList(
+                new SimplePojo("1"), new SimplePojo("2"), new SimplePojo("3"));
+        JsonApiDocument doc = JsonApiDocument.fromData(pojoList);
+        JsonNode result = mapper.valueToTree(doc);
+
+        JsonNode data = result.get("data");
+        assertEquals(JsonNodeType.ARRAY, data.getNodeType());
+        assertEquals(3, data.size());
+        assertEquals("1", data.get(0).get("id").textValue());
+        assertEquals("2", data.get(1).get("id").textValue());
+        assertEquals("3", data.get(2).get("id").textValue());
+        assertEquals("simple", data.get(0).get("type").textValue());
+        assertEquals(2, data.get(1).get("attributes").size());
+        assertEquals("something", data.get(2).get("attributes").get("anotherAttribute").textValue());
     }
 
 }
